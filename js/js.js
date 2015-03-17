c92eb5069a8b5ce5591053d245f628017cf88f77
Raxszeon <niklas.brandstrom@tshirt.se>
2015-03-17 18:41:09 +0100

    Pruttblöjja

--- js/js.js
+++ js/js.js
@@ -1,21 +1,22 @@
+var accessToken = '144505288.a880b37.f60727772f8e413dad52225165cd1a42';
+var clientId	= 'a880b37bc8c6409fa2fecd66255b7e44';
+var apiUri		= 'http://www.niklasbrandstrom.se/api/';
+
 function JSONPRequest(url) {
     var s = document.createElement('script');
     s.setAttribute('src', url);
     document.getElementsByTagName('head')[0].appendChild(s);
 	}
 
-
-var accessToken = '144505288.a880b37.f60727772f8e413dad52225165cd1a42';
-var userId 		= '144505288';
-var clientId	= 'a880b37bc8c6409fa2fecd66255b7e44';
-var apiUri		= 'http://www.niklasbrandstrom.se/api/';
-
-function getBilder(){ 	
-	JSONPRequest("https://api.instagram.com/v1/users/"+userId+"/media/recent/?access_token="+accessToken+"&callback=callbackBilder ");
+function getBilder(userId){ 	
+	JSONPRequest("https://api.instagram.com/v1/users/"+userId+"/media/recent/?access_token="+accessToken+"&count=18&callback=callbackBilder ");
 	}
 	function callbackBilder(response){
 		console.log(response);	
 		
+		// Nollställer bild-boxen
+		document.getElementById("bacon_bilder").innerHTML = "";
+
 		var data;
 		data=response.data;
 
@@ -24,26 +25,23 @@ function getBilder(){
 			
 			var holder=document.createElement('div');
 				holder.className="puff";
-
 			var inner=document.createElement('div');
 				inner.className="inner";
-
-			var created_time=document.createElement('span');
-				created_time.className="timestamp";
-			
-			var caption=document.createElement('span');
+			var caption=document.createElement('div');
 				caption.className="caption";
-
 			var images=document.createElement('img');
 			
-			created_time.innerHTML=data[i].created_time;
-			caption.innerHTML=data[i].caption.text;
-			
+			// Kontrollerar om bilden har kommentar eller ej
+			if(data[i].caption != null) { 
+				caption.innerHTML=data[i].caption.text;				
+				}
+
 			images.setAttribute('src',data[i].images.standard_resolution.url);
 			
 			holder.appendChild(inner);
 			inner.appendChild(images);
-			
+			inner.appendChild(caption);
+
 
 			document.getElementById('bacon_bilder').appendChild(holder);
 		}
@@ -52,24 +50,19 @@ function getBilder(){
 
 function getUserInfo (){
 	var username = document.getElementById('username').value;
-	var accessToken = '25113078.dbe2570.254bdb0509e44bfdbdd7810ddb68f219';
 	JSONPRequest('https://api.instagram.com/v1/users/search?q='+username+'&access_token='+accessToken+'&callback=callbackUserInfo');
-}
-
-function callbackUserInfo (response){
-	console.log(response);
-	
-	var data;
-	data = response.data;
-	
-	if (data[0].length = 1){
-		console.log('Retrieved the user ID from instagrams API');
-		userID = data[0].id;
-		console.log('user ID is: '+userID);
-		
-		getBilder();
-		
-	}else{
-		console.log('Could not retrieve the user ID from instagrams API');
 	}
+	function callbackUserInfo (response){
+		console.log(response);
+		var data;
+		data = response.data;	
+		if (data[0].length = 1){
+			console.log('Retrieved the user ID from instagrams API');
+			userID = data[0].id;
+			console.log('user ID is: '+userID);
+			getBilder(userID); // Hämtar och visar bilder
+			
+		}else{
+			console.log('Could not retrieve the user ID from instagrams API');
+		}
 }
